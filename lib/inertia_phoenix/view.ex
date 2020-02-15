0c3963791bd38c76172bff16a4303c3745499ca3
Troy Martin <troy@devato.com>
2020-02-15 11:35:09 -0500

    Add props

--- lib/inertia_phoenix/view.ex
+++ lib/inertia_phoenix/view.ex
@@ -1,12 +1,15 @@
 defmodule InertiaPhoenix.View do
   def render("inertia.html", assigns) do
-    Phoenix.HTML.Tag.content_tag(:div, "", [{:id, "app"}, {:data, [page: page_data()]}])
+    Phoenix.HTML.Tag.content_tag(:div, "", [
+      {:id, "app"},
+      {:data, [page: page_data(assigns)]}
+    ])
   end
 
-  defp page_data do
+  defp page_data(%{conn: conn}) do
     Jason.encode!(%{
       component: "Home",
-      props: %{hello: "world"},
+      props: conn.assigns.props,
       url: "/",
       version: "1.0"
     })
