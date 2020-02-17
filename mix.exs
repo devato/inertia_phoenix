e2cacdb5ee98ce541e7c9c14918e4b742652750c
Troy Martin <troy@devato.com>
2020-02-17 13:46:55 -0500

    Test coveralls

--- mix.exs
+++ mix.exs
@@ -10,6 +10,8 @@ defmodule InertiaPhoenix.MixProject do
       start_permanent: Mix.env() == :prod,
       compilers: [:phoenix] ++ Mix.compilers(),
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       description: "Inertiajs adapter for Elixir Phoenix",
       package: package()
     ]
@@ -32,7 +34,8 @@ defmodule InertiaPhoenix.MixProject do
       # Credo requires jason to exist also in :dev
       {:jason, "~> 1.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.21.0", only: :dev},
-      {:plug_cowboy, "~> 2.1", only: [:test]}
+      {:plug_cowboy, "~> 2.1", only: [:test]},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
