b3da2590c3062f115f3c78ea08697136603006a6
Nelson Luiz Ferraz de Camargo <ncamargo@othostelecom.co.za>
2020-02-17 20:47:01 +0200

    Implements #4 and lazy loading of props

--- lib/inertia_phoenix/controller.ex
+++ lib/inertia_phoenix/controller.ex
@@ -1,6 +1,7 @@
 defmodule InertiaPhoenix.Controller do
   @moduledoc false
   import InertiaPhoenix
+  import Plug.Conn, only: [get_req_header: 2]
   alias Phoenix.Controller
 
   def render_inertia(conn, component, assigns \\ [props: %{}])
@@ -8,6 +9,8 @@ defmodule InertiaPhoenix.Controller do
   def render_inertia(%{assigns: %{inertia_request: true}} = conn, component, assigns) do
     assigns =
       assigns
+      |> filter_partial_data(conn)
+      |> lazy_load()
       |> assign_component(component)
       |> assign_flash(Controller.get_flash(conn))
 
@@ -17,6 +20,8 @@ defmodule InertiaPhoenix.Controller do
   def render_inertia(conn, component, assigns) do
     assigns =
       assigns
+      |> filter_partial_data(conn)
+      |> lazy_load()
       |> assign_component(component)
       |> assign_flash(Controller.get_flash(conn))
 
@@ -45,5 +50,37 @@ defmodule InertiaPhoenix.Controller do
 
   defp assign_flash(assigns, flash) do
     put_in(assigns, [:props, :flash], flash)
+  end
+
+  defp filter_partial_data(assigns, conn) do
+    case get_req_header(conn, "x-inertia-partial-data") do
+      [] ->
+        assigns
+
+      [partial_data] ->
+        requested_props = String.split(partial_data, ",")
+
+        Keyword.put(
+          assigns,
+          :props,
+          assigns[:props]
+          |> Enum.filter(fn {k, _} -> Atom.to_string(k) in requested_props end)
+        )
+    end
+  end
+
+  defp lazy_load(assigns) do
+    Keyword.put(
+      assigns,
+      :props,
+      Enum.map(assigns[:props], fn {k, v} ->
+        if is_function(v) do
+          {k, v.()}
+        else
+          {k, v}
+        end
+      end)
+      |> Enum.into(%{})
+    )
   end
 end
