f4fe54767f62a180f614a990963761232dcfca9f
Troy Martin <troy@devato.com>
2020-02-18 10:07:31 -0500

    Remove unused config

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,3 @@
 Logger.configure(level: :warn)
 
 ExUnit.start()
-
-# {:ok, _pid} = InertiaPhoenix.Test.Endpoint.start_link()
-
-# # Make sure we can run distribution tests
-# :os.cmd('epmd -daemon')
