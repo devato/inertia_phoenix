0659f48e471314f17255ad114b57df6a30deb864
Troy Martin <troy@devato.com>
2020-02-21 09:28:37 -0500

    Fix for share

--- lib/inertia_phoenix.ex
+++ lib/inertia_phoenix.ex
@@ -1,41 +1,14 @@
 defmodule InertiaPhoenix do
   @moduledoc false
-
-  import Logger
   import Plug.Conn
 
   def share(%Plug.Conn{} = conn, key, val) do
     shared_props =
-      case get_session(conn, "inertia_phoenix_shared_props") do
-        nil ->
-          conn.private
-          |> Map.get(:inertia_phoenix_shared_props, %{})
-          |> Map.put(key, val)
+      conn.private
+      |> Map.get(:inertia_phoenix_shared_props, %{})
+      |> Map.put(key, val)
 
-        data ->
-          data
-      end
-
-    conn = put_private(conn, :inertia_phoenix_shared_props, shared_props)
-
-    register_before_send(conn, fn conn ->
-      shared_props = conn.private.inertia_phoenix_shared_props
-      props_size = map_size(shared_props)
-
-      cond do
-        is_nil(shared_props) and props_size == 0 ->
-          Logger.warn("=> cond 1\n")
-          conn
-
-        props_size > 0 and conn.status in 300..308 ->
-          Logger.warn("=> cond 2\n")
-          put_session(conn, "inertia_phoenix_shared_props", shared_props)
-
-        true ->
-          Logger.warn("=> cond 3\n")
-          delete_session(conn, "inertia_phoenix_shared_props")
-      end
-    end)
+    put_private(conn, :inertia_phoenix_shared_props, shared_props)
   end
 
   def assets_version do
