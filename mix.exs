cc205547359e1484ed8c554b9b56a48b9c95c1e1
Troy Martin <troy@devato.com>
2020-02-22 14:26:25 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule InertiaPhoenix.MixProject do
   def project do
     [
       app: :inertia_phoenix,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
