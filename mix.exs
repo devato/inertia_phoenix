9c32654965a949862a943f389ea105f8993fa658
tmartin8080 <troy@devato.com>
2023-07-11 10:08:15 -0400

    Bump elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule InertiaPhoenix.MixProject do
     [
       app: :inertia_phoenix,
       version: "0.4.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers(),
